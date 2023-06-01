c882fce80d545ef479cc62cb875250b3485d6045
Paweł Łęczycki <pawel@msd.info.pl>
2023-06-01 10:10:58 +0200

    Wprowadzenie do .NET 6

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+string? a = "ala";
+string b = ToUpper(a)!;
+
+
+string? ToUpper(string @string /*!! - null-checking feature - dodaje podczas kompilacji (niejawnie) kod jak poniżej*/)
+{
+/*    if (@string == null)
+        throw new ArgumentNullException(nameof(@string));*/
+
+    return @string?.ToUpper();
+}
+
+
+//namespace ConsoleApp
+//{
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+            //instrukcje najwyższego poziomu
+            Console.WriteLine("Hello, World!");
+
+ //       }
+//    }
+//}

b188be353e4d26a00192c3c6c5377b57c7998032
Paweł Łęczycki <pawel@msd.info.pl>
2023-06-02 09:28:56 +0200

    Budowa aplikacji

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -12,6 +12,68 @@ if (app.Environment.IsDevelopment()) {
 }
 
 
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Begin use1 {context.Request.Path}");
+    if (context.Request.Path.ToString().Contains(".ico"))
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+    else
+    {
+        await next();
+    }
+    Console.WriteLine($"End use1 {context.Request.Path}");
+});
+
+app.Map("/hello", helloApp =>
+{
+    helloApp.Use(async (context, next) =>
+    {
+        Console.WriteLine($"Begin mapUse1 {context.Request.Path}");
+
+
+        await next();
+        Console.WriteLine($"End mapUse1 {context.Request.Path}");
+    });
+
+
+    helloApp.Run(async context =>
+    {
+        Console.WriteLine($"Begin mapRun {context.Request.Path}");
+        await context.Response.WriteAsync("Hello anonymouse!");
+        Console.WriteLine($"End mapRun {context.Request.Path}");
+    });
+});
+
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Begin use2 {context.Request.Path}");
+
+
+    await next();
+    Console.WriteLine($"End use2 {context.Request.Path}");
+});
+
+
+app.MapWhen(context => context.Request.Query.TryGetValue("name", out _), mapWhen =>
+{
+    mapWhen.Run(async context =>
+    {
+        Console.WriteLine($"Begin mapWhenRun {context.Request.Path}");
+        await context.Response.WriteAsync($"Hello {context.Request.Query["name"]}!");
+        Console.WriteLine($"End mapWhenRun {context.Request.Path}");
+    });
+});
+
+
+app.Run(async context =>
+{
+    Console.WriteLine($"Begin run {context.Request.Path}");
+    await context.Response.WriteAsync("Hello World!");
+    Console.WriteLine($"End run {context.Request.Path}");
+});
+
+
 
 
 app.Run();
